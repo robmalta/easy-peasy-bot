565d77ebc85a43aba3506856e29c1f0d9949f77d
robmalta <rob.malta@gmail.com>
2019-01-31 02:22:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -89,6 +89,10 @@ controller.hears('hello', 'direct_message', function (bot, message) {
     bot.reply(message, 'Hello!');
 });
 
+controller.hears('420', function (bot, message) {
+    bot.reply(message, 'Blaze it!');
+});
+
 
 /**
  * AN example of what could be:
